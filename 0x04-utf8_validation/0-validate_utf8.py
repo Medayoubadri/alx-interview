d6e4ede810d324e39aa7a8572b21519983e0b3bd
medayoubadri <medayoubadri@gmail.com>
2025-05-12 18:12:57 +0100

    0x04-utf8_validation done

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+function validUTF8(data)
+that determines if a given data set represents a valid UTF-8 encoding.
+"""
+
+def validUTF8(data):
+    """ Check if a given data set represents a valid UTF-8 encoding """
+    num_bytes = 0
+
+    for byte in data:
+        # Get the number of bytes to expect
+        if num_bytes == 0:
+            if (byte >> 7) == 0b0:
+                continue
+            elif (byte >> 5) == 0b110:
+                num_bytes = 1
+            elif (byte >> 4) == 0b1110:
+                num_bytes = 2
+            elif (byte >> 3) == 0b11110:
+                num_bytes = 3
+            else:
+                return False
+        else:
+            # Check if the byte is a continuation byte
+            if (byte >> 6) != 0b10:
+                return False
+            num_bytes -= 1
+
+    return num_bytes == 0
